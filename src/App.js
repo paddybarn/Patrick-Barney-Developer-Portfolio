0f30b0eaef37f50e3b442a001d5e03edf732a2f8
paddybarn <107075686+paddybarn@users.noreply.github.com>
2022-10-04 23:29:57 -0400

    began adding starter code.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Project from './components/Project';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+<div>
+  <Header />
+  <Navigation />
+  <Project />
+  <Footer />
+</div>
+  )
+}
+
+export default App;
